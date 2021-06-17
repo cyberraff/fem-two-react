c9dcee9a5c890454365732488806af9529cd0811
Cyber Raff <cyberraff@gmail.com>
2021-06-17 06:12:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import sedans from './images/icon-sedans.svg';
+import suvs from './images/icon-suvs.svg';
+import luxury from './images/icon-luxury.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='main'>
+      <div className='sedans , sections'>
+        <img src={sedans} alt='sedans' />
+        <h1>Sedans</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button className='btn'>Learn More</button>
+      </div>
+
+      <div className='suvs , sections'>
+        <img src={suvs} alt='suvs' />
+
+        <h1>SUVs</h1>
+        <p>
+          Take an SUV for its spacious interior, power, and versatility. Perfect
+          for your next family vacation and off-road adventures.
+        </p>
+
+        <button className='btn'>Learn More</button>
+      </div>
+
+      <div className='luxury , sections'>
+        <img src={luxury} alt='luxury' />
+        <h1>Luxury</h1>
+        <p>
+          Cruise in the best car brands without the bloated prices. Enjoy the
+          enhanced comfort of a luxury rental and arrive in style.
+        </p>
+
+        <button className='btn'>Learn More</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
